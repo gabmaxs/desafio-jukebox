cbd52093b58ae301dacc7210c040852c5b0be926
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-10-27 10:41:13 -0300

    add component table

---
+++ vue/src/components/Table.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- COMECA TABLE -->
+    <div class="container">
+      <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>E-mail</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in list" v-bind:key="user.id">
+          <td>{{ user.id }}</td>
+          <td>{{ user.first_name }}</td>
+          <td>{{ user.email }}</td>
+          <td>
+            <div class="buttons is-centered are-small">
+                <button class="button is-warning" @click="$emit('edit', user.id)">Editar</button> 
+                <button class="button is-danger" @click="$emit('delete', user.id)">Excluir</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+    <!-- TERMINA TABLE -->
+</template>
+
+<script>
+export default {
+    name: "Table",
+    props: {
+        list: Array
+    },
+}
+</script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">Usuários Cadastrados</h1>
+    <Table :list="list" v-on:delete="deleteUser" v-on:edit="editUser" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios"
+import Table from "@/components/Table.vue"
 export default {
   name: 'Home',
+  data() {
+    return {
+      list: []
+    }
+  },
   components: {
-    HelloWorld
+    Table
+  },
+  methods: {
+    getList() {
+      console.log("buscando usuarios")
+      axios.get("/users", {
+        headers: {
+          "Accept": "application/json" 
+        }
+      })
+      .then(response => {
+        this.list = response.data.users
+      })
+      .catch(error => console.log(error.response))
+    },
+    deleteUser(user_id) {
+      axios.delete(`/users/${user_id}`, {
+        headers: {
+          "Accept": "application/json" 
+        }
+      })
+      .then(response => {
+        this.removeFromList(user_id)
+        console.log(response)
+      })
+      .catch(error => console.log(error.response))
+    },
+    removeFromList(user_id) {
+      this.list = this.list.filter(user => user.id != user_id)
+    },
+    editUser(user_id) {
+      this.$router.push({
+        name: 'Edit',
+        params: { id: user_id}
+      })
+    }
+  },
+  created() {
+    this.getList()
   }
 }
 </script>
