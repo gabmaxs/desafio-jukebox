137edce758eed2eefbe7b9a385916ae03f36a00f
Gabriel Maximiniano <gabmaxsan@gmail.com>
2020-10-26 22:05:00 -0300

    add register page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Página Inicial</router-link> |
+      <router-link to="/register">Novo Usuário</router-link>
     </div>
-    <router-view/>
+    <div class="container is-max-desktop">
+      <router-view/>
+    </div>
   </div>
 </template>
 
---
+++ vue/src/views/Register.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="register">
+    <h1>Registrar novo usuário</h1>
+    <form id="form" method="post">
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Nome</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input class="input" type="text" placeholder="Digite o nome">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Sobrenome</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input class="input" type="text" placeholder="Digite o sobrenome">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">E-mail</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input class="input" type="email" placeholder="Digite o e-mail">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Telefone</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input class="input" type="text" placeholder="Digite o telefone">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label">
+          <label class="label">Pessoa Jurídica?</label>
+        </div>
+        <div class="field-body">
+          <div class="field is-narrow">
+            <p class="control">
+              <label class="radio">
+                <input type="radio" name="member">
+                Sim
+              </label>
+              <label class="radio">
+                <input type="radio" checked name="member">
+                Não
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">CPF</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input class="input" type="text" placeholder="Digite o CPF">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">CNPJ</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input class="input" type="text" placeholder="Digite o CNPJ">
+            </p>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
